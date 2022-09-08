7c0483096e0ccfc44b4991020a089668e7044c64
joud <joudhaybi@gmail.com>
2022-09-08 03:02:50 +0300

    css

--- WebApplication2/JS/Mainjs.js
+++ WebApplication2/JS/Mainjs.js
@@ -2,78 +2,71 @@
 var Cardtodo = [];
 
 $(document).ready(function () {
-    $("#SerachInput").keyup(function () {
-            $("tbody").html("");
-        
-    var Search = $(this).val();
-    if (Search == null) {
-        Search="";
-    }
-            console.log(Search);
-            $.ajax({
-                type: "POST",
-                dataType: 'json',
-                url: "../Home/GetTodo",
-                data: { Search: Search },
-                success: function (data) {
-                    console.log(data.data);
-                    //var archieve = JSON.parse(data);
-                    if (data.data.length) {
+    $.ajax({
+        type: "POST",
+        dataType: 'json',
+        url: "../Home/GetTodo",
+        data: { Search: "" },
+        success: function (data) {
+            console.log(data.data);
+            //var archieve = JSON.parse(data);
+            if (data.data.length) {
 
-                        for (var i = 0; i < data.data.length; i++) {
+                for (var i = 0; i < data.data.length; i++) {
 
-                            Cardtodo = '<div class="card cardstyle inputstyle " id="cardid' + data.data[i].TodoID + '">';
-                            Cardtodo += '<div class="card-body">';
-                            Cardtodo += '<div class="form-group row">';
-                            Cardtodo += '<div class="col-sm-10">';
-                            Cardtodo += '<input class="card-title Titleinputfield inputstyle cardinputtitle" id="titleid' + data.data[i].TodoID + '" type="text"  value="' + data.data[i].Title + '" readonly>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '<div class="form-group row">';
-                            Cardtodo += '<label  class="col-sm-2 col-form-label">Category</label>';
-                            Cardtodo += '<div class="col-sm-10">';
-                            Cardtodo += '<input type="text" class="catinputfield inputstyle cardinputs" id="catid' + data.data[i].TodoID + '" value="' + data.data[i].Category + '" readonly>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '<div class="form-group row">';
-                            Cardtodo += '<label class="col-sm-2 col-form-label">DueDate</label>';
-                            Cardtodo += '<div class="col-sm-10">';
-                            Cardtodo += '<input type="date" class="dateinputfield inputstyle cardinputs" id="duedateid' + data.data[i].TodoID + '" value="' + data.data[i].DueDate + '" readonly>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '<div class="form-group row">';
-                            Cardtodo += '<label class="col-sm-2 col-form-label">Estimate</label>';
-                            Cardtodo += '<div class="col-sm-10">';
-                            Cardtodo += '<input type="text" class="Estimateinputfield inputstyle cardinputs" id="estimateid' + data.data[i].TodoID + '" value="' + data.data[i].Estimate + '" readonly>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '<div class="form-group row">';
-                            Cardtodo += '<label class="col-sm-2 col-form-label">Importance</label>';
-                            Cardtodo += '<div class="col-sm-10">';
-                            Cardtodo += '<input type="text" class="Impoinputfield inputstyle cardinputs" id="impoid' + data.data[i].TodoID + '" value="' + data.data[i].ImportanceName + '" readonly>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '</div>';
-                            Cardtodo += '</div></div>';
-                            $("tbody").append(Cardtodo);
+                    Cardtodo = '<div class="card cardstyle inputstyle " id="cardid' + data.data[i].TodoID + '">';
+                    Cardtodo += '<div class="card-body">';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input class="card-title Titleinputfield inputstyle cardinputtitle" id="titleid' + data.data[i].TodoID + '" type="text"  value="' + data.data[i].Title + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label  class="col-sm-2 col-form-label">Category</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="text" class="catinputfield inputstyle cardinputs" id="catid' + data.data[i].TodoID + '" value="' + data.data[i].Category + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label class="col-sm-2 col-form-label">DueDate</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="date" class="dateinputfield inputstyle cardinputs" id="duedateid' + data.data[i].TodoID + '" value="' + data.data[i].DueDate + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label class="col-sm-2 col-form-label">Estimate</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="text" class="Estimateinputfield inputstyle cardinputs" id="estimateid' + data.data[i].TodoID + '" value="' + data.data[i].Estimate + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label class="col-sm-2 col-form-label">Importance</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="text" class="Impoinputfield inputstyle cardinputs" id="impoid' + data.data[i].TodoID + '" value="' + data.data[i].ImportanceName + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div></div>';
+                    $("tbody").append(Cardtodo);
 
-                            if ($("#impoid" + data.data[i].TodoID).val() == "Hight") {
-                                $("#impoid" + data.data[i].TodoID).css("background-color", "#DC3545");
-                            } else if ($("#impoid" + data.data[i].TodoID).val() == "Medium") {
-                                $("#impoid" + data.data[i].TodoID).css("background-color", "#FE913E");
-                            } else {
-                                $("#impoid" + data.data[i].TodoID).css("background-color", "#39AC95");
-                            }
-
-                        }
+                    if ($("#impoid" + data.data[i].TodoID).val() == "Hight") {
+                        $("#impoid" + data.data[i].TodoID).css("background-color", "#DC3545");
+                    } else if ($("#impoid" + data.data[i].TodoID).val() == "Medium") {
+                        $("#impoid" + data.data[i].TodoID).css("background-color", "#FE913E");
+                    } else {
+                        $("#impoid" + data.data[i].TodoID).css("background-color", "#39AC95");
                     }
 
-                },
-                error: function () {
-                    alert("Error");
                 }
-            });
-        
+            }
+
+        },
+        error: function () {
+            alert("Error");
+        }
     });
+     
+
+
     if ($(".Impoinputfield ").val() == "Hight") {
         $(".Impoinputfield ").css("background-color", "#DC3545");
     }
@@ -145,7 +138,78 @@ $(document).ready(function () {
     });
 
 });
+$("#SerachInput").keyup(function () {
+    $("tbody").html("");
 
+    var Search = $(this).val();
+    if (Search == null) {
+        Search = "";
+    }
+
+    $.ajax({
+        type: "POST",
+        dataType: 'json',
+        url: "../Home/GetTodo",
+        data: { Search: Search },
+        success: function (data) {
+            console.log(data.data);
+            //var archieve = JSON.parse(data);
+            if (data.data.length) {
+
+                for (var i = 0; i < data.data.length; i++) {
+
+                    Cardtodo = '<div class="card cardstyle inputstyle " id="cardid' + data.data[i].TodoID + '">';
+                    Cardtodo += '<div class="card-body">';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input class="card-title Titleinputfield inputstyle cardinputtitle" id="titleid' + data.data[i].TodoID + '" type="text"  value="' + data.data[i].Title + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label  class="col-sm-2 col-form-label">Category</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="text" class="catinputfield inputstyle cardinputs" id="catid' + data.data[i].TodoID + '" value="' + data.data[i].Category + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label class="col-sm-2 col-form-label">DueDate</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="date" class="dateinputfield inputstyle cardinputs" id="duedateid' + data.data[i].TodoID + '" value="' + data.data[i].DueDate + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label class="col-sm-2 col-form-label">Estimate</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="text" class="Estimateinputfield inputstyle cardinputs" id="estimateid' + data.data[i].TodoID + '" value="' + data.data[i].Estimate + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '<div class="form-group row">';
+                    Cardtodo += '<label class="col-sm-2 col-form-label">Importance</label>';
+                    Cardtodo += '<div class="col-sm-10">';
+                    Cardtodo += '<input type="text" class="Impoinputfield inputstyle cardinputs" id="impoid' + data.data[i].TodoID + '" value="' + data.data[i].ImportanceName + '" readonly>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div>';
+                    Cardtodo += '</div></div>';
+                    $("tbody").append(Cardtodo);
+
+                    if ($("#impoid" + data.data[i].TodoID).val() == "Hight") {
+                        $("#impoid" + data.data[i].TodoID).css("background-color", "#DC3545");
+                    } else if ($("#impoid" + data.data[i].TodoID).val() == "Medium") {
+                        $("#impoid" + data.data[i].TodoID).css("background-color", "#FE913E");
+                    } else {
+                        $("#impoid" + data.data[i].TodoID).css("background-color", "#39AC95");
+                    }
+
+                }
+            }
+
+        },
+        error: function () {
+            alert("Error");
+        }
+    });
+
+});
 function selectimportance(data) {
     ImportanceID = data;
 }
